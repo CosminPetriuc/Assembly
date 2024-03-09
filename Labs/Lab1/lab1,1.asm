7cbd6cadcfb2e2f89d9a87525341ed819b5af4e1
CosminPetriuc <122042873+CosminPetriuc@users.noreply.github.com>
2024-03-09 19:50:54 +0200

    Add files via upload

---
+++ Labs/Lab1/lab1,1.asm
@@ -0,0 +1,56 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ;declarare variabile
+    a dw 7
+    b db 6
+    c dw 3
+    d db 4
+    ; e = a-b+7-(c+d)
+    
+    
+    
+
+; our code starts here
+segment code use32 class=code
+    start:
+        ;e = a-b+7-(c+d)
+        ;a-b
+        ;word - byte
+        ;b trebuie sa devina word
+        mov bl, [b]
+        ;[] pentru a accesa valoarea unei variabile
+        ;bx = bh|bl
+        mov bh, 0
+        ;bx = b
+        mov ax, [a]
+        sub ax, bx; ax = a - b
+        
+        
+        add ax, 7; ax = 1+7
+        
+        ; c+d
+        ;word + byte
+        ;d trebuie sa devina word
+        mov dl, [d]
+        ;[] pentru a accesa valoarea unei variabile
+        ;dx = dh|dl
+        mov dh, 0 
+        ; dx = d
+        add dx , [c] ; dx = 3 + 4
+        
+        sub ax, dx; e = 8-7
+        
+    
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
---
+++ Labs/Lab1/lab1.asm
@@ -0,0 +1,66 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ; declare variables
+    a db 10
+    b dw 5
+    ; fara semn
+
+; our code starts here
+segment code use32 class=code
+    start:
+        ; codul care solutioneaza o problema 
+        ; adunarea add opd, ops;
+                               ;opd = opd+ops
+                               ;restrictii:
+                                    ;1. ops nu poate fi constanta 
+                                    ;2. opd, ops sa fie de aceasi dimensiune (acelasi tip de data)
+                                    ;3. cel putin un operand sa fie reg sau constanta
+                                    ;add 3,4 - nu se poate
+                                    ;add 5, al - nu se poate
+        ;e = a + b
+        ; a trebuie sa fie word
+        mov al, [a]
+        ;[] pentru a acesa valoarea unei variabile
+        ;ax = ah|al
+        mov ah, 0
+        ;ax = a 
+        add ax, [b] 
+        ;ax = 10 + 5
+        
+        ;scadere sub opd, ops
+        ;opd = opd-ops
+        ;restrictii:
+                        ;1. opd nu poate fi constanta
+                        ;2. opd, ops sa fie de aceasi dimensiune(acelasi tip de data)
+                        ;3. cel putin un operand sa fie reg sau constanta
+                        
+        ; a + b = ax
+            ; -c
+            ; ax - c
+            ;word - doubleword
+            ;16 biti - 32 biti
+            ; ax trebuie sa devina doubleword
+        ; movzx op1, op2
+        ; op1 se completeaza la stanga cu valoarea 0 prin extinderea op2 la dim opl
+        ; op1 este intotdeauna de dim mai mare decat op2
+        ; op1 este intotdeauna registru
+        ; op2 poate sa fie registru sau variabia ( nu poate sa fie constanta)
+        
+        ;extindem ax la eax folosind movzx
+        movzx eax, ax; se completeaza partea high a lui eax cu valoarea 0
+        
+        sub eax, [c];eax = eax - c
+    
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
