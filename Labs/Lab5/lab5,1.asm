689f1256d26d4f75b0f0aa50dcb7dac307712632
CosminPetriuc <122042873+CosminPetriuc@users.noreply.github.com>
2024-03-09 19:54:26 +0200

    Add files via upload

---
+++ Labs/Lab5/lab5,1.asm
@@ -0,0 +1,45 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ; B7. A string of doublewords T is given. Compute string R containing only high bytes from high words from each doubleword from string S.
+
+    ;If S = 12345678h, 1a2b3c4dh => D = 12h, 1ah
+    s dd 12345678h, 1a2b3c4dh 
+    ls equ ($-s)/4 ; 1 doubleword = 4 bytes
+    r resw ls ; Result array
+    
+; our code starts here
+segment code use32 class=code
+    start:
+        mov esi, 0 ; Index for source string S
+        mov edi, 0 ; Index for destination string R
+        mov ecx, ls ; Number of doublewords
+
+    repeta:
+        mov eax, [s + esi] ; Load a doubleword from S into eax
+        shr eax, 16 ; Shift right to get the high word
+        and eax, 0FF00h ; Mask to keep only the high byte
+
+        cmp eax, 12h ; Compare with 12h
+        jne NEXT ; Jump to NEXT if not equal
+
+        ; If equal, store the doubleword in R
+        mov [r + edi], eax
+        add edi, 4 ; Move to the next position in R (4 bytes per doubleword)
+
+    NEXT:
+        add esi, 4 ; Move to the next doubleword in S
+        loop repeta ; Repeat for the next doubleword
+    
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
---
+++ Labs/Lab5/lab5.asm
@@ -0,0 +1,42 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ;A3. A string of bytes A is given. Construct string B such that each element from B represent the remainder for the division of two consecutive elements from string A.
+
+    ;If A = 16, 8, 3 => B = 0, 2
+     A db 16, 8, 3    ; Define array A
+
+; our code starts here
+segment code use32 class=code
+    start:
+        mov ecx, 0       ; Initialize loop counter
+
+    compute_remainders:
+        mov al, [A + ecx]   ; Load the current element from A into al
+        mov bl, [A + ecx + 1] ; Load the next element from A into bl
+
+        xor ah, ah          ; Clear ah register for division
+        div bl              ; Divide al by bl, result in al, remainder in ah
+
+        mov [B + ecx], ah   ; Store the remainder in B
+
+        inc ecx             ; Move to the next pair of elements in A
+
+        cmp ecx, 2          ; 3 elements - 1
+        jl compute_remainders  ; If ecx < 2, continue looping
+
+        ; Terminate the program using exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
+
+    section .bss
+        B resb 2          ; Reserve space for array B
